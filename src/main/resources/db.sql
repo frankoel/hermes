daf6cef13017fee18fd7c724058f97850a183bea
Francisco Jose Coello Canales <fcoello@addocean.com>
2023-10-16 12:18:45 +0200

    Database inserts

---
+++ src/main/resources/db.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.company
+(active, code, "name")
+VALUES(true, '11112222A', 'TEST');
+
+
+INSERT INTO public.user_data
+(active, "admin", code, "name", "password", company_id)
+VALUES(true, true, '75763090D', 'Fran', 'LAQUESEA', 1);
